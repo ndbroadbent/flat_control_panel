7cfae26d8a2cf148fd24edfcd6d1c7d3a11ee7c6
Nathan Broadbent <nathan.f77@gmail.com>
2010-12-06 12:34:54 +1300

    Added error catching, infinitely loops on error in case reader is unplugged.

--- octopus_daemon.rb
+++ octopus_daemon.rb
@@ -14,29 +14,34 @@ require 'uri'
 config = YAML.load_file relative("config.yml")
 base_url = URI.parse(config['sinatra_base_url'])
 
-# Loop until all devices are connected
-o_connect = false
-
-while not o_connect
-  begin
-    @octopus = Octopus.new config["octopus_devnode"], 9600, 14
-    @octopus.reset    # Tries to send reset string to octopus reader. Raises error if not connected.
-    o_connect = true
-  rescue
-    puts "Waiting for octopus card reader to be available..."
-    sleep 2
-  end
-end
-
-
 while true
-  id = @octopus.read
+  begin
+    # Loop until all devices are connected
+    o_connect = false
 
-  # After a tag is read, send it to the sinatra server for validation and processing.
-  # Response doesnt matter.
-  res = Net::HTTP.start(base_url.host, base_url.port) {|http|
-    http.get("/octopus/#{id}")
-  }
+    while not o_connect
+      begin
+        @octopus = Octopus.new config["octopus_devnode"], 9600, 14
+        @octopus.reset    # Tries to send reset string to octopus reader. Raises error if not connected.
+        o_connect = true
+      rescue
+        puts "Waiting for octopus card reader to be available..."
+        sleep 2
+      end
+    end
 
+    while true
+      id = @octopus.read
+
+      # After a tag is read, send it to the sinatra server for validation and processing.
+      # Response doesnt matter.
+      res = Net::HTTP.start(base_url.host, base_url.port) {|http|
+        http.get("/octopus/#{id}")
+      }
+
+    end
+  rescue
+    puts "Octopus was disconnected. Looping until connected again."
+  end
 end
 
