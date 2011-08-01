2a32a990f8577c5bc730d4de4f326589b10ade94
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-01 12:40:29 +0800

    update time once per minute, instead of once per second.

--- octopus_sinatra.rb
+++ octopus_sinatra.rb
@@ -82,9 +82,7 @@ def lcd_default   # Default lcd display
     while true
       time = hk_time
       $dsp420.write lcd_time_fmt(time, ":"), 14, 20, false
-      sleep 1
-      $dsp420.write lcd_time_fmt(time, " "), 14, 20, false
-      sleep 1
+      sleep 60
       # If its the start of a new day, refresh the message.
       if time.hour == 0 and time.min == 0 and time.sec < 3
         $dsp420.write motd, 21, 40, false
